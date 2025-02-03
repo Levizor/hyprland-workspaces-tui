a6a29ea84bc55d7f53712e092354d6a8cff0dad2
Levizor <levizorri@gmail.com>
2025-02-03 12:58:19 +0100

    remove build file

--- build.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    if Command::new("hyprland-workspaces").output().is_err() {
-        eprintln!("Error: 'hyprland-workspaces' is not installed or not in PATH");
-        std::process::exit(1);
-    }
-}
